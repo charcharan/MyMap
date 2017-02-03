d7d2022e4bfc3ea9b9734dfb057cdc470cc9b78a
unknown <charan49075@gmail.com>
2017-02-03 20:39:34 +0530

    modificatons

--- js/app.js
+++ js/app.js
@@ -2,28 +2,85 @@ var map;
 var markers = [];
 var largeInfowindow;
 var infowindow;
-var locations = [
-    { title: 'Nellore', location: { lat: 14.442599, lng: 79.986456 } },
-    { title: 'Golconda Fort', location: { lat: 17.385363, lng: 78.40413 } },
-    { title: 'Tirumala', location: { lat: 13.678184, lng: 79.332357 } },
-    { title: 'Hampi', location: { lat: 15.335013, lng: 76.460025 } },
-    { title: 'Papikondalu', location: { lat: 17.479712, lng: 81.493313 } },
-    { title: 'Simhachalam', location: { lat: 17.774162, lng: 83.231863 } },
-    { title: 'Madurai', location: { lat: 9.925201, lng: 78.119775 } },
-    { title: 'Rameswaram', location: { lat: 9.287625, lng: 79.312929 } },
+var locations = [{
+        title: 'Nellore',
+        location: {
+            lat: 14.442599,
+            lng: 79.986456
+        }
+    },
+    {
+        title: 'Golconda Fort',
+        location: {
+            lat: 17.385363,
+            lng: 78.40413
+        }
+    },
+    {
+        title: 'Tirumala',
+        location: {
+            lat: 13.678184,
+            lng: 79.332357
+        }
+    },
+    {
+        title: 'Hampi',
+        location: {
+            lat: 15.335013,
+            lng: 76.460025
+        }
+    },
+    {
+        title: 'Papi Hills',
+        location: {
+            lat: 17.479712,
+            lng: 81.493313
+        }
+    },
+    {
+        title: 'Simhachalam',
+        location: {
+            lat: 17.774162,
+            lng: 83.231863
+        }
+    },
+    {
+        title: 'Madurai',
+        location: {
+            lat: 9.925201,
+            lng: 78.119775
+        }
+    },
+    {
+        title: 'Rameswaram',
+        location: {
+            lat: 9.287625,
+            lng: 79.312929
+        }
+    },
 ];
+var marker;
 
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 14.462188158, lng: 79.99894781 },
+        center: {
+            lat: 14.462188158,
+            lng: 79.99894781
+        },
         zoom: 13
     });
 
     largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
 
-
+    var marker = new google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
+    });
 
     for (var i = 0; i < locations.length; i++) {
         // Get the position from the location array.
@@ -32,7 +89,7 @@ function initMap() {
         // locations[i].marker = marker;
 
         // Create a marker per location, and put into markers array.
-        var marker = new google.maps.Marker({
+        marker = new google.maps.Marker({
             map: map,
             position: position,
             title: title,
@@ -59,48 +116,49 @@ var ViewModel = function() {
 
     this.locationList = ko.observableArray(locations);
 
+    // Push
+
     this.filteredList = ko.computed(function() {
         // if the filter is empty, return every elements and show every markers
         if (self.currentFilter().length === 0) {
-          // TODO: loop through every locations and show their markers
-          for (var i = 0; i < self.locationList().length; i++) {
-            // save the current iteratee as "loc" for easier reference
-            loc = self.locationList()[i];
-            console.log(loc);
-            if (loc.marker) {
-              console.log('hi');
-            }
-            // loc.marker.setVisible(true);
-          }
-          // self.locationList()[3].marker.setVisible(true);
-          // return the entire location list from the comp. observable
-          return self.locationList();
+            // TODO: loop through every locations and show their markers
+            // for (var i = 0; i < self.locationList().length; i++) {
+            //   // save the current iteratee as "loc" for easier reference
+            //
+            //
+            //   loc = self.locationList()[i];
+            //   console.log(loc);
+            //
+            //   loc.marker.setVisible(true);
+            // }
+            // // self.locationList()[3].marker.setVisible(true);
+            // return the entire location list from the comp. observable
+            return self.locationList();
         }
 
         // create a temporary array - we will return it from the computed observable
         var tempArray = [];
         // loop through the locationList
         for (var i = 0; i < self.locationList().length; i++) {
-          // save the current iteratee as "loc" for easier reference
-          loc = self.locationList()[i];
-          // if the filter is inside the title...
-          // OPTIONAL TODO: convert title and the currentFilter
-          // to lowercase before comparing them
-          if (loc.title.indexOf(self.currentFilter()) !== -1) {
-            // add the location to the temporary array
-            tempArray.push(loc);
-            // show the marker
-            loc.marker.setVisible(true);
-          } else {
-            // hide the marker
-            loc.marker.setVisible(false);
-          }
+            // save the current iteratee as "loc" for easier reference
+            loc = self.locationList()[i];
+            // if the filter is inside the title...
+            // OPTIONAL TODO: convert title and the currentFilter
+            // to lowercase before comparing them
+            if (loc.title.toLowerCase().indexOf(self.currentFilter()) !== -1) {
+                // add the location to the temporary array
+                tempArray.push(loc);
+                // show the marker
+                loc.marker.setVisible(true);
+            } else {
+                // hide the marker
+                loc.marker.setVisible(false);
+            }
         }
         // then return the temporary array from the comp. observable
         // so it's value will be the filtered list
         return tempArray;
     }, this);
-
 
     this.setLoc = function(clickedLoc) {
 
@@ -109,10 +167,6 @@ var ViewModel = function() {
         console.log(clickedLoc);
 
     };
-
-
-
-
 
 };
 
@@ -124,48 +178,46 @@ ko.applyBindings(new ViewModel());
 function populateInfoWindow(marker, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
 
+    var articleUrl;
+    var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
+    var wikiTimeout = setTimeout(function() {
+        alert("failed to load wikipedia page");
+    }, 8000);
+    $.ajax({
+        url: wikiURL,
+        dataType: "jsonp"
+        //jsnop datatype
+    }).success(function(response) {
+        //timeout is cleared if wikipedia link is loaded successfully
+        clearTimeout(wikiTimeout);
+        var info;
         var articleUrl;
-        var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
-        var wikiTimeout = setTimeout(function () {
-           alert("failed to load wikipedia page");
-       }, 8000);
-       $.ajax({
-           url: wikiURL,
-           dataType: "jsonp"
-           //jsnop datatype
-       }).success(function(response) {
-           //timeout is cleared if wikipedia link is loaded successfully
-           clearTimeout(wikiTimeout);
-           var articleUrl;
 
-           //response from wikipedia api
-           articleUrl = response[2];
-           if (infowindow.marker != marker) {
+        //response from wikipedia api
+        info = response[2];
+        articleUrl = response[3];
+        if (infowindow.marker != marker) {
 
-               infowindow.marker = marker;
-               infowindow.setContent('<div>' + marker.title + '</div><br><a href ="' + articleUrl + '">' + articleUrl + '</a><hr>');
-               infowindow.open(map, marker);
-               marker.setAnimation(google.maps.Animation.BOUNCE);
-               setTimeout(function() {
-                   marker.setAnimation(null);
-               }, 3000);
+            infowindow.marker = marker;
+            infowindow.setContent('<div>' + marker.title + '</div><p>' + info + '</p><a href ="https://en.wikipedia.org/wiki/' + marker.title + '">go to wikipedia</a>');
+            infowindow.open(map, marker);
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() {
+                marker.setAnimation(null);
+            }, 3000);
 
+            // TODO: Create an AJAX request to a third party site (e.g. wikipedia)
+            // ex: $.ajax({construct the query here})
+            // .done(function(response) {})
+            // .fail(function(error) {});
 
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function() {
+                // infowindow.setMarker(null);
+                infowindow.marker = null;
+            });
 
-
-        // TODO: Create an AJAX request to a third party site (e.g. wikipedia)
-        // ex: $.ajax({construct the query here})
-        // .done(function(response) {})
-        // .fail(function(error) {});
-
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick', function() {
-            // infowindow.setMarker(null);
-            infowindow.marker = null;
-        });
-
-    }
-           });
+        }
+    });
 
 }
-var loc;
